e3b8cd316a958e6756ad4fe421e873fe279a12fe
Mark Chindudzi <chindudzim@africau.edu>
2025-08-13 14:25:19 +0200

    final touchs to my django webpage

--- Django-Portfolio-WebApp/nature_webpage/main/views.py
+++ Django-Portfolio-WebApp/nature_webpage/main/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project, Tag
 
-# Create your views here.
 
 def home(request):
-  return render(request, 'home.html')
-
-def contact(request):
-  return render(request, 'contact.html')
-
-def  projects(request):
     projects = Project.objects.all()
     tags = Tag.objects.all()
-    
-    context = {
-        'projects': projects,
-        'tags': tags
-    }
-    
-    return render(request, 'projects.html', context)
+    return render(request, "home.html", {"projects": projects, "tags": tags})
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def project(request, id):
+    project = get_object_or_404(Project, pk=id)
+    return render(request, "project.html", {"project": project})
